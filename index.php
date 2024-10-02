d292d3ec301a54027d5b7efdbc4febd884a1ac29
ChristopherS24 <chryskies@gmail.com>
2024-10-02 18:14:47 +0200

    stringa inserita in api + collegamenti

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<?php
-    $columnsNumber = 4;
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+        <title>PHP Dischi JSON</title>
 
         <!-- Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -18,28 +15,27 @@
                 <div class="row">
                     <div class="col text-center">
                         <h1>
-                            Template PHP
+                        PHP Dischi JSON
                         </h1>
                     </div>
                 </div>
             </div>
         </header>
 
-        <main>
+        <main id="app">
             <div class="container">
                 <div class="row">
-                    <?php
-                        for ($i = 0; $i < $columnsNumber; $i++) {
-                    ?>
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                            Colonna <?php echo $i + 1; ?>
-                        </div>
-                    <?php
-                        }
-                    ?>
+                    {{ message }}
                 </div>
             </div>
         </main>
+
+<!--Vue-->
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<!--Axios-->
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<!--JS-->
+<script src="js/scripts.js"></script>
 
     </body>
 </html>
